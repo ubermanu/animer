ec1d28afe20f734ecfdd24358a1ee758448462fd
ubermanu <manu@paca.happy-dev.fr>
2021-05-02 16:34:48 +0200

    Move scripts into the pages

--- src/panel.js
+++
@@ -1,9 +0,0 @@
-import Animer from './Animer.svelte'
-
-chrome.devtools.inspectedWindow.eval(
-  'document.getAnimations()',
-  function (animations) {
-    console.log(animations)
-    new Animer({ target: document.body, props: { animations } })
-  }
-)
