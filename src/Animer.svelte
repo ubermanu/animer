9cd75ecb21ab8524f106bfb8f606016e56fb302d
ubermanu <manu@paca.happy-dev.fr>
2021-05-02 13:37:53 +0200

    Render details of the animation

--- src/Animer.svelte
+++ src/Animer.svelte
@@ -1,1 +1,27 @@
-<p>Animer</p>
+<script>
+    const element = document.querySelector('.example')
+    const animations = element.getAnimations()
+
+    console.log(animations)
+    // console.log(getComputedStyle(element))
+</script>
+
+{#each animations as animation}
+    <div class="animation">
+        <h4>
+            <span>{animation.animationName}</span>
+            <small>({animation.playState})</small>
+        </h4>
+        <button on:click={() => animation.play()}>Play</button>
+        <button on:click={() => animation.pause()}>Pause</button>
+    </div>
+    <div class="keyframes">
+        {#each animation.effect.getKeyframes() as keyframe}
+            <ul>
+                {#each Object.entries(keyframe) as [prop, value]}
+                    <li>{prop}: {value}</li>
+                {/each}
+            </ul>
+        {/each}
+    </div>
+{/each}
